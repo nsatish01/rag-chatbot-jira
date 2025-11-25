36b763706367ae3a354ef05eb47b3620cbc9751c
nsatish01 <20325711+nsatish01@users.noreply.github.com>
2025-11-25 14:29:17 -0500

    update 11252025

--- src/chunk_and_embed.py
+++ src/chunk_and_embed.py
@@ -1,9 +1,28 @@
 
 from transformers import AutoTokenizer
 import pandas as pd
+import ssl
+import certifi
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+import os
+os.environ["HF_HUB_DISABLE_SSL_VERIFICATION"] = "1"
+
+from huggingface_hub import hf_hub_download
+
+
+
+
+hf_hub_download(
+    repo_id="meta-llama/Llama-2-7b-hf",
+    filename="tokenizer_config.json",
+    local_dir=".",
+    force_download=True,
+    resume_download=True,
+    token="YOUR_TOKEN"
+)
 
 # Load your CSV
-jira_df = pd.read_csv("C:\\Users\\snagarajan\\AI_learnings\\rag-chatbot-jira\\data\\sample_jira_tickets.csv")
+jira_df = pd.read_csv("C:\\Users\\snagarajan\\AI_learnings\\rag-chatbot-jira\\data\\sample_jira_tickets.csv",on_bad_lines="skip")  # skips problematic rows)
 
 # Initialize tokenizer for LLaMA 2 (or any HF model you want)
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")  # adjust model name as needed
